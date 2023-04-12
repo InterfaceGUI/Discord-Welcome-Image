d04f69655de56ec744e28d00e23e412c360c8670
larshagrid <interfacegui@gmail.com>
2023-04-12 16:15:33 +0800

    upload
    src

---
+++ card.js
@@ -0,0 +1,200 @@
+const {registerFont,loadImage,createCanvas} = require("canvas");
+const { resolve } = require("path");
+const StackBlur = require('stackblur-canvas');
+var OpenType = require('opentype.js');
+
+async function CreateBackground(bg,boderblur=0,bgblur=0){
+    const oimage = await loadImage(bg)
+
+    const canvas = createCanvas(700, 250);
+    const ctx = canvas.getContext('2d');
+    
+    ctx.drawImage(oimage, 0, 0, canvas.width, canvas.height);
+
+    StackBlur.canvasRGBA(canvas, 0, 0, canvas.width, canvas.height, boderblur);
+  
+    // 绘制圆角矩形
+    const radius = 35; // 圆角半径
+    ctx.globalCompositeOperation = 'destination-in';
+    ctx.beginPath();
+    ctx.moveTo(radius, 0);
+    ctx.lineTo(canvas.width - radius, 0);
+    ctx.quadraticCurveTo(canvas.width, 0, canvas.width, radius);
+    ctx.lineTo(canvas.width, canvas.height - radius);
+    ctx.quadraticCurveTo(canvas.width, canvas.height, canvas.width - radius, canvas.height);
+    ctx.lineTo(radius, canvas.height);
+    ctx.quadraticCurveTo(0, canvas.height, 0, canvas.height - radius);
+    ctx.lineTo(0, radius);
+    ctx.quadraticCurveTo(0, 0, radius, 0);
+    ctx.closePath();
+    ctx.fill();
+    ctx.clip();
+
+    // 添加邊框
+    const borderWidth = 15; // 邊框寬度
+    ctx.lineWidth = borderWidth;
+    ctx.strokeStyle = '#000';
+    ctx.stroke(); 
+
+    ctx.globalCompositeOperation = 'destination-over';
+    
+    const canvas1 = createCanvas(canvas.width, canvas.height);
+    const ctx1 = canvas1.getContext('2d');
+    ctx1.drawImage(oimage, 0, 0, canvas1.width, canvas1.height);
+
+    StackBlur.canvasRGBA(canvas1, 0, 0, canvas1.width, canvas1.height, bgblur);
+
+    ctx.drawImage(canvas1, 0, 0, canvas.width, canvas.height);
+    return canvas
+}
+
+async function addAvatar(bgcanvas,avatars){
+    const circleX = bgcanvas.width/5
+    const circleY = bgcanvas.height/2
+    const circleRadius = 100
+
+    const canvas = createCanvas(bgcanvas.width, bgcanvas.height);
+    const ctx = canvas.getContext('2d');
+
+    const gradient = ctx.createLinearGradient(0, circleY+circleRadius/2,circleRadius+circleRadius/2);
+    gradient.addColorStop(0, '#33f');
+    gradient.addColorStop(1, '#f33');
+
+
+    ctx.beginPath();
+    ctx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fillStyle = '#fff';
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = '#fff';//gradient;
+    ctx.stroke();
+    ctx.fill();
+    ctx.clip();
+
+    const avatar = await loadImage(avatars)
+
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.drawImage(avatar, circleX - circleRadius, circleY - circleRadius, circleX + circleRadius + 0, circleY + circleRadius + 0);
+
+
+
+    const ctx2 = bgcanvas.getContext('2d');
+    ctx2.globalCompositeOperation = 'source-over';
+    ctx2.drawImage(canvas,0,0)
+    return bgcanvas
+}
+
+async function addTitle(c,welcomefont,welcomeText,welcomeTextFontSize=34,Titlefont,text,fontSize = 52,subTitleFont,subTitle,subTitleFontSize){
+    const TextX = (c.width/5) * 2
+    const TextY = (c.height/2) + (fontSize/2)
+    const canvas = createCanvas(c.width, c.height);
+    const ctx = canvas.getContext('2d');
+
+    var path = Titlefont.getPath(text,TextX,TextY,fontSize);    
+
+    path.draw(ctx);
+
+    ctx.fillStyle = '#fff';
+    ctx.strokeStyle = '#F3F3F3';
+    ctx.lineWidth = 1.5;
+    ctx.stroke();
+    ctx.fill();
+
+    welcomefont.getPath(welcomeText,TextX, TextY - fontSize - 5, welcomeTextFontSize).draw(ctx)
+
+    ctx.fillStyle = '#fff';
+    ctx.lineWidth = 1.5;
+    ctx.strokeStyle = '#000';
+    ctx.fill();
+
+
+    subTitleFont.getPath(subTitle,TextX,(c.height/4)*3.2,subTitleFontSize).draw(ctx)
+
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+
+    const ctx2 = c.getContext('2d');
+    ctx2.globalCompositeOperation = 'source-over';
+    ctx2.drawImage(canvas,0,0)
+
+    return c
+
+}
+
+
+/**
+ * @typedef {Object} CardOptions
+ * @property {string} [WelcomeTextFont] - The blur of boder.
+ * @property {string} [Titlefont] - The blur of boder.
+ * @property {string} [SubTitleFont] - The blur of boder.
+ * 
+ * @typedef {Object} BackgroundOptions
+ * @property {number} [boderblur] - The blur of boder.
+ * @property {number} [bgblur] - The blur of the background.
+ * 
+ * @typedef {Object} TextSizeOptions
+ * @property {number} [WelcomeTextSize] - 
+ * @property {number} [TitleSize] - 
+ * @property {number} [SubtitleSize] - 
+ */
+class WelcomeCard{
+    /**
+    * @param {CardOptions} [options] - The options for the person.
+    */
+    constructor(options = {}){
+        this.WelcomeTextFontPath =  options.WelcomeTextFont || "./fonts/NaikaiFont-Bold.ttf"
+        this.TitlefontPath =  options.Titlefont || "./fonts/NotoSansTC-Black.otf"
+        this.SubTitleFontPath =  options.SubTitleFont || "./fonts/NaikaiFont-Bold.ttf"
+    }
+    async LoadFonts(){
+        this.WelcomeTextFont = await OpenType.load(this.WelcomeTextFontPath)
+        this.Titlefont = await OpenType.load(this.TitlefontPath)
+        this.subTitleFont = await OpenType.load(this.SubTitleFontPath)
+    }
+    /**
+    * Create a Image.
+    * @param {BackgroundOptions} [bgOptions] - The options for the person.
+    * @param {TextSizeOptions} [textOptions] - The options for the person.
+    */
+    async CreateImageCard(BackgroundImg, Avatar, WelcomeText, Title, Subtitle, bgOptions={},textOptions={}){
+        const BackgroundOptions1 = {
+            boderblur: bgOptions.boderblur || 90,
+            bgblur: bgOptions.bgblur || 8
+        }
+        const TextSize ={
+            WelcomeTextSize: textOptions.WelcomeTextSize || 34,
+            TitleSize: textOptions.TitleSize || 62,
+            SubtitleSize:textOptions.SubtitleSize || 24
+        }
+
+
+        const welcomefont = this.WelcomeTextFont
+        const Titlefont = this.Titlefont
+        const SubTitleFont = this.subTitleFont
+    
+        const c_bg = await CreateBackground(BackgroundImg, BackgroundOptions1.boderblur, BackgroundOptions1.bgblur)
+        const c_avatar = await addAvatar(c_bg, Avatar);
+        const c_Title = await addTitle(c_avatar, welcomefont, WelcomeText,TextSize.WelcomeTextSize, Titlefont, Title, TextSize.TitleSize, SubTitleFont, Subtitle, TextSize.SubtitleSize)
+    
+        return c_Title.toBuffer('image/png')
+    }
+    
+}
+
+async function CreateImageCard(bg,avatar,WelcomeText,Title,Subtitle){
+
+    const welcomefont = await OpenType.load('./fonts/NaikaiFont-Bold.ttf')
+    const Titlefont = await OpenType.load('./fonts/NotoSansTC-Black.otf')
+    const SubTitleFont = await OpenType.load('./fonts/NaikaiFont-Bold.ttf')
+
+    const c_bg = await CreateBackground(bg, 90, 8)
+    const c_avatar = await addAvatar(c_bg, avatar);
+    const c_Title = await addTitle(c_avatar, welcomefont, WelcomeText, Titlefont, Title, 62, SubTitleFont, Subtitle, 24)
+
+    return c_Title.toBuffer('image/png')
+
+}
+
+module.exports = { WelcomeCard,CreateImageCard }
+
+
---
+++ index.js
@@ -0,0 +1,69 @@
+//const config = require('./config.json')
+
+const { Client, GatewayIntentBits, Events} = require('discord.js');
+
+const {WelcomeCard} = require('./card.js');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+const config={
+    token: process.env.TOKEN,
+    GuildID: process.env.GUILDID,
+    WelcomeChannel: process.env.W_CHANNEL,
+    infoChannel: process.env.INFO_CHANNEL,
+    WelcomeText: process.env.WTEXT,
+    memberCountText: process.env.MTEXT
+}
+
+const wCard = new WelcomeCard({
+    WelcomeTextFont: "./fonts/NaikaiFont-Bold.ttf",
+    Titlefont: "./fonts/NotoSansTC-Black.otf",
+    SubTitleFont: "./fonts/NaikaiFont-Bold.ttf"
+})
+
+
+const bg = 'https://media.discordapp.net/attachments/464402915591979028/1095545854917623848/PSX_20230412_110801.jpg'
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async message => {
+    if (message.author.bot) return;
+    if (!(['KarterLauder#2466','LarsHagrid#2620'].includes(message.author.tag))) return;
+    if (!message.content.startsWith('w!test1')) return;
+	const avatar = message.author.displayAvatarURL({ extension: 'png' })
+    const imagebuffer = await wCard.CreateImageCard(
+        bg,
+        avatar,
+        '歡迎誤入拉斯的狼窩',
+        message.author.username,
+        `狼仔數量: ${message.guild.memberCount}`
+    )
+    
+    await message.channel.send({files: [imagebuffer] })
+
+});
+
+client.on(Events.GuildMemberAdd, async member  => {
+    
+	const avatar = member.displayAvatarURL({ extension: 'png' })
+    const imagebuffer = await wCard.CreateImageCard(
+        bg,
+        avatar,
+        `${config.WelcomeText}`,
+        member.username,
+        `${config.memberCountText}: ${member.guild.memberCount}`
+    )
+    
+    const channel = await client.guilds.cache.get(config.GuildID).channels.fetch(config.WelcomeChannel)
+    await channel.send({files: [imagebuffer] });
+
+    const channel2 = await client.guilds.cache.get(config.GuildID).channels.fetch(config.infoChannel)
+    await channel2.send({content:`<@${member.id}>`});
+
+});
+wCard.LoadFonts().then(
+    client.login(config.token)
+)
